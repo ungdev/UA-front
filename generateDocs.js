54f000ee5198b1962ddcc58d0c8da888e5c38411
Florian Esslinger <florian.esslinger@live.fr>
2019-08-26 11:54:59 +0200

    feat: enhance doc

---
+++ generateDocs.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const fs = require('fs');
+const reactDocgen = require('react-docgen');
+const ReactDocGenMarkdownRenderer = require('react-docgen-markdown-renderer');
+
+/* Add component name in the list */
+const componentsList = ['Button', 'Modal', 'Title'];
+componentsList.forEach((component) => {
+  const componentPath = path.join(__dirname, `components/UI/${component}.jsx`);
+  const renderer = new ReactDocGenMarkdownRenderer();
+  fs.readFile(componentPath, (error, content) => {
+    const documentationPath = `docs/${component}${renderer.extension}`;
+    const doc = reactDocgen.parse(content);
+    fs.writeFile(documentationPath, renderer.render(
+      componentPath,
+      doc,
+      [],
+    ), () => {});
+  });
+});
