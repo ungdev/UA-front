822b5abd665a608846e0ba011c5df2963579510f
Florian Esslinger <florian.esslinger@live.fr>
2019-09-05 23:28:25 +0200

    feat: legal page

--- pages/legal.js
+++ pages/legal.js
@@ -1,3 +1,30 @@
-const Legal = () => 'Legal';
+import React from 'react';
+import { Title } from '../components';
+
+import text from '../assets/text';
+import './legal.css';
+
+const Legal = () => (
+  <div id="legal">
+    <Title align="center">mentions légales</Title>
+    <Title level={2}>Propriétaire du site</Title>
+    {text.legal.ung}
+    <Title level={2}>Hébergeur du site</Title>
+    {text.legal.ung}
+    <Title level={2}>Informations relatives aux cookies</Title>
+    <p>{text.legal.cookies}</p>
+    <Title level={2}>Conditions d&apos;accès aux systèmes informatiques</Title>
+    <p>{text.legal.acces}</p>
+    <Title level={2}>Conditions générales de vente</Title>
+    <div>
+      {text.legal.vente.map(({ title, content }) => (
+        <>
+          <Title level={3}>{title}</Title>
+          <p>{content}</p>
+        </>
+      ))}
+    </div>
+  </div>
+);
 
 export default Legal;
