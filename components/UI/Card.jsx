212963776ec7af0b8039d11818330fa6a523afed
Florian Esslinger <florian.esslinger@live.fr>
2019-09-13 15:21:57 +0200

    feat: partners page

--- components/UI/Card.jsx
+++ components/UI/Card.jsx
@@ -7,27 +7,32 @@ import 'lazysizes/plugins/attrchange/ls.attrchange';
 import Button from './Button';
 import './Card.css';
 
-const Card = ({ imgSrc, content, textButton, onClick, link }) => {
+const Card = ({ imgSrc, content, textButton, onClick, link, classNameCard, classNameImg }) => {
   const button = <Button primary onClick={onClick}>{textButton}</Button>;
   return (
-    <div className="card-card">
-      {imgSrc && <img className="lazyload card-img" alt="" data-src={imgSrc} />}
-      <div className="card-content">
-        {content}
-      </div>
-      <div className="card-button">
-        {textButton && link ? <Link href={link} key={link}>{button}</Link> : button}
-      </div>
+    <div className={`card-card ${classNameCard}`}>
+      {imgSrc && <img className={`lazyload card-img ${classNameImg}`} alt="" data-src={imgSrc} />}
+      { content
+        && (
+        <>
+          <div className="card-content">{content}</div>
+          <div className="card-button">
+            {textButton && link ? <Link href={link} key={link}>{button}</Link> : button}
+          </div>
+        </>
+        ) }
     </div>
   );
 };
 
 Card.propTypes = {
   imgSrc: PropTypes.string,
-  content: PropTypes.object.isRequired,
+  content: PropTypes.object,
   textButton: PropTypes.string,
   onClick: PropTypes.func,
   link: PropTypes.string,
+  classNameCard: PropTypes.string,
+  classNameImg: PropTypes.string,
 };
 
 Card.defaultProps = {
@@ -35,6 +40,9 @@ Card.defaultProps = {
   textButton: undefined,
   onClick: () => {},
   link: undefined,
+  content: undefined,
+  classNameCard: '',
+  classNameImg: '',
 };
 
 export default Card;
--- pages/partners.js
+++ pages/partners.js
@@ -1,3 +1,24 @@
-const Partners = () => 'Partners';
+import React from 'react';
+import { Card, Header, Title } from '../components';
+import partnersList from '../assets/partners.json';
+import './partners.css';
 
-export default Partners;
+const partners = () => (
+  <div>
+    <Header />
+    <div id="partners">
+      <Title align="center" uppercase>Partenaires</Title>
+      <div className="list-partners">
+        { partnersList.map((partner) => (
+          <div className="partner">
+            <a href={partner.link}>
+              <Card imgSrc={partner.img} classNameCard="white-card" classNameImg="partner-img" />
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
+);
+
+export default partners;
