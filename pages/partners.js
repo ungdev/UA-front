1b9198d3c844382c130bac079ec3479ed6050b8c
Valentin Peltier <valentinpeltier@yahoo.fr>
2019-09-13 23:43:26 +0200

    fix: misc

--- pages/partners.js
+++ pages/partners.js
@@ -3,24 +3,26 @@ import { Card, Header, Title } from '../components';
 import partnersList from '../assets/partners.json';
 import './partners.css';
 
-const listPartners = partnersList.map((partner) => (
-  <div className="partner" key={partner.link}>
-    <a href={partner.link}>
-      <Card imgSrc={partner.img} classNameCard="white-card" classNameImg="partner-img" />
-    </a>
-  </div>
-));
+const Partners = () => {
+  const listPartners = partnersList.map((partner) => (
+    <div className="partner" key={partner.link}>
+      <a href={partner.link}>
+        <Card imgSrc={partner.img} classNameCard="white-card" classNameImg="partner-img" />
+      </a>
+    </div>
+  ));
 
-const Partners = () => (
-  <div>
-    <Header />
-    <div id="partners">
-      <Title align="center" uppercase>Partenaires</Title>
-      <div className="list-partners">
-        { listPartners }
+  return (
+    <div>
+      <Header />
+      <div id="partners">
+        <Title align="center" uppercase>Partenaires</Title>
+        <div className="list-partners">
+          { listPartners }
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Partners;
