d989821b1420f652ea73a1a07528e454549f5085
Valentin Peltier <valentinpeltier@yahoo.fr>
2019-09-16 17:10:53 +0200

    fix: service worker (this time it will work)

--- static/service-worker.js
+++ static/service-worker.js
@@ -103,7 +103,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = '/service-worker.js';
+      const swUrl = '/static/service-worker.js';
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
