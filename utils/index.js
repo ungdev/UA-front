5cb68bb50f1525fbcabf0591bc4139505febb939
Valentin <valentinpeltier@yahoo.fr>
2019-09-29 04:51:33 +0200

    feat: add CookieConsent component

--- utils/index.js
+++ utils/index.js
@@ -36,4 +36,20 @@ const axiosAPI = (token = '') => {
   }
 };
 
-export { postToSlack, axiosAPI };
+const getCookie = (cookieName) => {
+  var name = cookieName + '=';
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+};
+
+export { postToSlack, axiosAPI, getCookie };
