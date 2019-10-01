4637308908acb178e1fc06bcb531ea1dc30aba03
Valentin <valentinpeltier@yahoo.fr>
2019-10-01 18:47:57 +0200

    fix partners

--- assets/partners.jsx
+++ assets/partners.jsx
@@ -8,7 +8,11 @@ export default [
     img: '/static/partners/festival.jpg',
     description: (
       <>
-        <p>L’univers du jeu s’installe de nouveau à Troyes pour une 5<sup>ème</sup> année encore plus animée et avec davantage d’exposants ! C’est le rendez-vous des gamers de toutes les générations : des amateurs aux passionnés de jeux anciens, fantastiques ou traditionnels ! Avec plus de 6000m², le festival vous propose de découvrir trois grands univers :</p>
+        <p>
+          L’univers du jeu s’installe de nouveau à Troyes pour une 5<sup>ème</sup> année encore plus animée et avec davantage d’exposants !
+          C’est le rendez-vous des gamers de toutes les générations : des amateurs aux passionnés de jeux anciens, fantastiques ou traditionnels !
+          Avec plus de 6000m², le festival vous propose de découvrir trois grands univers :
+        </p>
         <ul>
           <li>Gaming, rétrogaming et fantastique</li>
           <li>Jeux de café et jeux traditionnels</li>
@@ -21,51 +25,93 @@ export default [
     name: 'ROG',
     link: 'https://rog.asus.com/',
     img: '/static/partners/rog.jpg',
-    description: <p>Réputée pour la fiabilité, la qualité et les performances de ses produits, la marque Asus est le n°1 mondial des fabricants de cartes mères. N°1 du PC Gaming, Asus Republic of Gamers est en quête constante de perfection technologique en concevant des ordinateurs, composants et périphériques pour PC sans cesse primés ! Toujours à l’avant-garde du secteur numérique, les produits Asus ont toujours été conçus pour satisfaire tous les niveaux de productivité et de divertissement.</p>,
+    description: (
+      <p>
+        Réputée pour la fiabilité, la qualité et les performances de ses produits, la marque Asus est le n°1 mondial des fabricants de cartes mères.
+        N°1 du PC Gaming, Asus Republic of Gamers est en quête constante de perfection technologique en concevant des ordinateurs, composants et périphériques pour PC sans cesse primés !
+        Toujours à l’avant-garde du secteur numérique, les produits Asus ont toujours été conçus pour satisfaire tous les niveaux de productivité et de divertissement.
+      </p>
+    ),
   },
   {
     name: 'Le Cube Troyes Champagne Expo',
     link: 'http://www.maisonduboulanger.com/missions/le-cube-troyes-champagne-expo/',
     img: '/static/partners/cube.jpg',
-    description: <p>Entièrement rénové en 2009, le Cube Troyes Champagne Expo est le site où se déroule la majorité des événements grand public (salons, concerts, spectacles, manifestation sportives…) et professionnels (congrès, conventions, galas, séminaires). Situé au cœur de la ville de Troyes, c’est le site événementiel qui accueille le Festival des Jeux et l’UTT Arena.</p>,
+    description: (
+      <p>
+        Entièrement rénové en 2009, le Cube Troyes Champagne Expo est le site où se déroule la majorité des événements grand public
+        (salons, concerts, spectacles, manifestation sportives…) et professionnels (congrès, conventions, galas, séminaires).
+        Situé au cœur de la ville de Troyes, c’est le site événementiel qui accueille le Festival des Jeux et l’UTT Arena.
+      </p>
+    ),
   },
   {
     name: 'War Legend',
     link: 'https://www.warlegend.net/',
     img: '/static/partners/warlegend.jpg',
-    description: <p>Structure francophone très active dans les jeux de l’esport les plus funs et populaires, War Legend est partenaire de l’UTT Arena pour l’organisation du tournoi Fortnite ! Leur équipe, habituée aux événements LAN apportera l’expérience pour vous proposer un tournoi inoubliable !</p>,
+    description: (
+      <p>
+        Structure francophone très active dans les jeux de l’esport les plus funs et populaires, War Legend est partenaire de l’UTT Arena pour l’organisation du tournoi Fortnite !
+        Leur équipe, habituée aux événements LAN apportera l’expérience pour vous proposer un tournoi inoubliable !
+      </p>
+    ),
   },
   {
     name: 'Blade',
     link: 'https://shadow.tech',
     img: '/static/partners/shadow.jpg',
-    description: <p>Shadow est la solution révolutionnaire que propose la start-up française Blade ! C'est le premier PC gaming dématérialisé et accessible avec un accès internet depuis un téléphone, une tablette, un PC, etc. Contre un abonnement mensuel, ce sont des performances exceptionnelles, un système à jour et des composants à la pointe de la technologie, accessibles à n'importe quel moment de la journée ! Le rêve des gamers ! <br />
-      Nous sommes membres de la troisième édition du Shadow University Program et nous sommes fiers de faire partie de la Team Shadow ! Pour en savoir plus, <a href="http://shdw.me/sup"> rendez-vous sur leur page</a> ! <br />
-      Shadow soutient l'UTT Arena et revient cette année avec de nombreuses suprises pour les joueurs et une dotation extra pour notre tombola ! </p>,
+    description: (
+      <p>
+        Shadow est la solution révolutionnaire que propose la start-up française Blade !
+        C'est le premier PC gaming dématérialisé et accessible avec un accès internet depuis un téléphone, une tablette, un PC, etc.
+        Contre un abonnement mensuel, ce sont des performances exceptionnelles, un système à jour et des composants à la pointe de la technologie,
+        accessibles à n'importe quel moment de la journée ! Le rêve des gamers !<br />
+        Nous sommes membres de la troisième édition du Shadow University Program et nous sommes fiers de faire partie de la Team Shadow !
+        Pour en savoir plus, <a href="http://shdw.me/sup">rendez-vous sur leur page</a> !<br />
+        Shadow soutient l'UTT Arena et revient cette année avec de nombreuses suprises pour les joueurs et une dotation extra pour notre tombola !
+      </p>
+    ),
   },
   {
     name: 'SilverStone',
     link: 'https://www.silverstonetek.com/index.php?area=fr',
     img: '/static/partners/silverstone.jpg',
-    description: <p> SilverStone voit le jour en 2003 avec une équipe d’ingénieurs issus d’un gros fabricant de matériel informatique. Depuis, la marque est parvenue à développer une très large gamme de produits allant des boitiers PC, boitiers gaming et mini-ITX, ventilateurs, alimentations, périphériques de stockage aux accessoires ! Cette diversité associée à l’expertise de l’équipe ont rapidement fait de la marque un acteur de premier plan sur le marché du hardware, mais aussi auprès des joueurs et passionnés d’informatique qui trouvent chez le fabricant tout le nécessaire pour leur PC. Les produits Silverstone séduisent par leur esthétique travaillée, une excellente qualité de fabrication et une finition impeccable.</p>,
+    description: (
+      <p>
+        SilverStone voit le jour en 2003 avec une équipe d’ingénieurs issus d’un gros fabricant de matériel informatique.
+        Depuis, la marque est parvenue à développer une très large gamme de produits allant des boitiers PC, boitiers gaming et mini-ITX,
+        ventilateurs, alimentations, périphériques de stockage aux accessoires !<br />
+        Cette diversité associée à l’expertise de l’équipe ont rapidement fait de la marque un acteur de premier plan sur le marché du hardware,
+        mais aussi auprès des joueurs et passionnés d’informatique qui trouvent chez le fabricant tout le nécessaire pour leur PC.
+        Les produits Silverstone séduisent par leur esthétique travaillée, une excellente qualité de fabrication et une finition impeccable.
+      </p>
+    ),
    },
   {
     name: 'La Grosse Ligue',
     link: 'https://fr.euw.leagueoflegends.com/university',
     img: '/static/partners/gl.jpg',
-    description: <p> La Grosse Ligue est la compétition officielle de Riot Games réservée aux étudiant.e.s résidant.e.s en France et administrée par le Student Gaming Network ! <br />
-      Chaque équipe sera reversée dans un palier de niveau (Carapateur, Héraut, Dragon, et Baron au niveau national), en fonction du niveau de chacun de ses joueurs.euses. Chaque semaine, les équipes d'une même région s'affronteront. Les meilleures équipes représenteront leur école au niveau national et les top 16 équipes seront diffusées sur O’Gaming !</p>,
+    description: (
+      <p>
+        La Grosse Ligue est la compétition officielle de Riot Games réservée aux étudiant.e.s résidant.e.s en France et administrée par le Student Gaming Network !<br />
+        Chaque équipe sera reversée dans un palier de niveau (Carapateur, Héraut, Dragon, et Baron au niveau national), en fonction du niveau de chacun de ses joueurs.euses.
+        Chaque semaine, les équipes d'une même région s'affronteront. Les meilleures équipes représenteront leur école au niveau national et les top 16 équipes seront diffusées sur O’Gaming !
+      </p>
+    ),
    },
   {
     name: 'Bureau Des Étudiants de l\'Université de Technologie de Troyes',
     link: 'https://bde.utt.fr/fr',
     img: '/static/partners/bde.jpg',
-    description: <p>Le BDE de l'UTT est la première association de notre l'école ! Le BDE remplit différentes missions :<br />
-      <ul>
-        <li>Fédérer et encourager la vie associative à l'UTT,</li>
-        <li>Animer le campus,</li>
-        <li>Permettre aux étudiants de s'épanouir pendant leur vie étudiante à l'UTT !</li>
-      </ul>
-    </p>,
+    description: (
+      <>
+        <p>Le BDE de l'UTT est la première association de notre l'école ! Le BDE remplit différentes missions :</p>
+        <ul>
+          <li>Fédérer et encourager la vie associative à l'UTT</li>
+          <li>Animer le campus</li>
+          <li>Permettre aux étudiants de s'épanouir pendant leur vie étudiante à l'UTT !</li>
+        </ul>
+      </>
+      ),
    },
 ];
