20ade2828aa85a037d35d93a0b15f020d36a39ef
Thomas de Lachaux <thomas.delachaux@gmail.com>
2019-10-01 18:17:41 +0200

    Eslint fix

--- assets/partners.jsx
+++ assets/partners.jsx
@@ -42,22 +42,22 @@ export default [
     description: <p>Shadow est la solution révolutionnaire que propose la start-up française Blade ! C'est le premier PC gaming dématérialisé et accessible avec un accès internet depuis un téléphone, une tablette, un PC, etc. Contre un abonnement mensuel, ce sont des performances exceptionnelles, un système à jour et des composants à la pointe de la technologie, accessibles à n'importe quel moment de la journée ! Le rêve des gamers ! <br />
       Nous sommes membres de la troisième édition du Shadow University Program et nous sommes fiers de faire partie de la Team Shadow ! Pour en savoir plus, <a href="http://shdw.me/sup"> rendez-vous sur leur page</a> ! <br />
       Shadow soutient l'UTT Arena et revient cette année avec de nombreuses suprises pour les joueurs et une dotation extra pour notre tombola ! </p>,
-  }, 
+  },
   {
     name: 'SilverStone',
     link: 'https://www.silverstonetek.com/index.php?area=fr',
     img: '/static/partners/silverstone.jpg',
-    description: <p> SilverStone voit le jour en 2003 avec une équipe d’ingénieurs issus d’un gros fabricant de matériel informatique. Depuis, la marque est parvenue à développer une très large gamme de produits allant des boitiers PC, boitiers gaming et mini-ITX, ventilateurs, alimentations, périphériques de stockage aux accessoires ! Cette diversité associée à l’expertise de l’équipe ont rapidement fait de la marque un acteur de premier plan sur le marché du hardware, mais aussi auprès des joueurs et passionnés d’informatique qui trouvent chez le fabricant tout le nécessaire pour leur PC. Les produits Silverstone séduisent par leur esthétique travaillée, une excellente qualité de fabrication et une finition impeccable.</p>
+    description: <p> SilverStone voit le jour en 2003 avec une équipe d’ingénieurs issus d’un gros fabricant de matériel informatique. Depuis, la marque est parvenue à développer une très large gamme de produits allant des boitiers PC, boitiers gaming et mini-ITX, ventilateurs, alimentations, périphériques de stockage aux accessoires ! Cette diversité associée à l’expertise de l’équipe ont rapidement fait de la marque un acteur de premier plan sur le marché du hardware, mais aussi auprès des joueurs et passionnés d’informatique qui trouvent chez le fabricant tout le nécessaire pour leur PC. Les produits Silverstone séduisent par leur esthétique travaillée, une excellente qualité de fabrication et une finition impeccable.</p>,
    },
   {
     name: 'La Grosse Ligue',
     link: 'https://fr.euw.leagueoflegends.com/university',
     img: '/static/partners/gl.jpg',
     description: <p> La Grosse Ligue est la compétition officielle de Riot Games réservée aux étudiant.e.s résidant.e.s en France et administrée par le Student Gaming Network ! <br />
-      Chaque équipe sera reversée dans un palier de niveau (Carapateur, Héraut, Dragon, et Baron au niveau national), en fonction du niveau de chacun de ses joueurs.euses. Chaque semaine, les équipes d'une même région s'affronteront. Les meilleures équipes représenteront leur école au niveau national et les top 16 équipes seront diffusées sur O’Gaming !</p>
+      Chaque équipe sera reversée dans un palier de niveau (Carapateur, Héraut, Dragon, et Baron au niveau national), en fonction du niveau de chacun de ses joueurs.euses. Chaque semaine, les équipes d'une même région s'affronteront. Les meilleures équipes représenteront leur école au niveau national et les top 16 équipes seront diffusées sur O’Gaming !</p>,
    },
   {
-    name: 'Bureau Des Étudiants de l\Université de Technologie de Troyes',
+    name: 'Bureau Des Étudiants de l\'Université de Technologie de Troyes',
     link: 'https://bde.utt.fr/fr',
     img: '/static/partners/bde.jpg',
     description: <p>Le BDE de l'UTT est la première association de notre l'école ! Le BDE remplit différentes missions :<br />
@@ -66,6 +66,6 @@ export default [
         <li>Animer le campus,</li>
         <li>Permettre aux étudiants de s'épanouir pendant leur vie étudiante à l'UTT !</li>
       </ul>
-    </p>
+    </p>,
    },
 ];
