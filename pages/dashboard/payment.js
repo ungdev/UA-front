57b38d694df969e590e52c50d7401433a92d1969
Florian Esslinger <florian.esslinger@live.fr>
2019-10-06 19:18:37 +0200

    feat: payment return page

---
+++ pages/dashboard/payment.js
@@ -0,0 +1,33 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
+
+const payment = () => {
+  const { query, push } = useRouter();
+  useEffect(() => {
+    if (query.type === 'success') {
+      toast.success('Paiement effectué avec succèes');
+    }
+    else if (query.type === 'error') {
+      switch (query.error) {
+        case 'CART_NOT_FOUND':
+          toast.error('Panier introuvable');
+          break;
+        case 'TRANSCATION_ERROR':
+          toast.error('La transcation a échoué');
+          break;
+        case 'NO_PAYLOAD':
+          toast.error('Mauvaise requete');
+          break;
+        default:
+          break;
+      }
+    }
+    push('/dashboard');
+  }, []);
+
+  return null;
+};
+
+export default payment;
