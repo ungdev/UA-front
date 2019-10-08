a2e4faf53fbd7dadda21f38a32f44bf597058adb
Florian Esslinger <florian.esslinger@live.fr>
2019-10-08 17:35:55 +0200

    feat: purchases list

--- pages/dashboard/purchases.js
+++ pages/dashboard/purchases.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-const Purchases = () => (
-  <div id="dashboard-purchases">
-    Purchases
-  </div>
-);
+import { Table, Card } from '../../components/UI';
+import { fetchAllCarts } from '../../modules/carts';
+import './purchases.css';
+
+const columns = [
+  { title: '', key: 'name' },
+  { title: 'Quantité', key: 'quantity' },
+  { title: 'Prix unitaire', key: 'price' },
+  { title: 'Sous total', key: 'total' },
+];
+
+const Purchases = () => {
+
+  const dispatch = useDispatch();
+  const carts = useSelector((state) => state.carts.allCarts.filter((cart) => cart.transactionState === 'paid'));
+  useEffect(() => {
+    dispatch(fetchAllCarts());
+  }, []);
+
+  const displayCarts = carts.map((cart) => {
+    const dataSource = cart.cartItems.map((cartItem) => ({
+      name: cartItem.item.name,
+      quantity: cartItem.quantity,
+      price: `${cartItem.item.price} €`,
+      total: `${cartItem.quantity * cartItem.item.price} €`,
+      totalInt: cartItem.quantity * cartItem.item.price,
+    }));
+    const total = dataSource.reduce((previousValue, data) => previousValue += data.totalInt, 0);
+    return (
+      <Card className="card-cart" key={cart.id} content={
+        <>
+          <p>Date: {cart.paidAt}</p>
+          <Table columns={columns} dataSource={dataSource}/>
+          <p className="cart-total"><strong>Total: {total} €</strong></p>
+        </>
+      }/>
+    );
+  });
+
+  return (
+    <div id="dashboard-purchases">
+      {displayCarts}
+    </div>
+  );
+};
 
 export default Purchases;
