927c0c541a7a125bd8d4329c27b623217642070a
Valentin <valentinpeltier@yahoo.fr>
2019-10-10 10:39:11 +0200

    fix Card warning

--- utils/errorToString.js
+++ utils/errorToString.js
@@ -1,19 +1,27 @@
 export default (error) => {
+  console.log(error);
   switch (error) {
     case 'DISABLED_LOGIN':
       return 'Inscription désactivée';
     case 'INVALID_FORM':
-      return 'Formulaire incomplet';
+      if (error.details) {
+        return error.details.map((detail) => {
+          return 'salut, ';
+        });
+      }
+      else {
+        return 'Formulaire invalide';
+      }
     case 'PASSWORD_MISMATCH':
-      return 'Les mots de passe ne correspondent pas';
+      return 'Les deux mots de passe ne correspondent pas';
     case 'INVALID_USERNAME':
       return 'Nom d\'utilisateur invalide';
+    case 'INVALID_EMAIL':
+      return 'E-mail introuvable';
     case 'INVALID_PASSWORD':
       return 'Mot de passe invalide';
     case 'ALREADY_IN_TEAM':
       return 'Vous êtes déjà dans une équipe';
-    case 'INVALID_EMAIL':
-      return 'E-mail introuvable';
     case 'INVALID_TOKEN':
       return 'Jeton invalide';
     case 'USER_NOT_ACTIVATED':
@@ -26,12 +34,12 @@ export default (error) => {
       return 'Le pseudo ou le mail est déjà utilisé';
     case 'VISITOR_FULL':
       return 'Il n\'y a plus de places visiteur';
-    case 'UNKNOWN':
-      return 'Une erreur est survenue';
     case 'LAN_FULL':
       return 'Il n\'y a plus de place pour l\'UTT Arena...';
     case 'PAYMENT_DISABLED':
       return 'Les paiements en ligne ont été désactivés';
+    case 'UNKNOWN':
+      return 'Une erreur est survenue';
     default:
       return error;
   }
