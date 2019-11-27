6d4a8963315ef751c4cf570bbed9c6123cc49d05
nyc4m <baptiste.prunot@utt.fr>
2019-11-27 17:55:44 +0100

    Change appId for one signal

--- modules/login.js
+++ modules/login.js
@@ -1,19 +1,19 @@
-import { toast } from 'react-toastify';
-import Router from 'next/router';
+import { toast } from "react-toastify";
+import Router from "next/router";
 
-import { setLoginModalVisible } from './loginModal';
-import { API, setTokenAPI } from '../utils';
-import { SET_CART } from './cart';
+import { setLoginModalVisible } from "./loginModal";
+import { API, setTokenAPI } from "../utils";
+import { SET_CART } from "./cart";
 
-export const SET_TOKEN = 'login/SET_TOKEN';
-export const SET_USER = 'login/SET_USER';
-export const UPDATE_USER = 'login/UPDATE_USER';
-export const SET_LOADING = 'login/SET_LOADING';
+export const SET_TOKEN = "login/SET_TOKEN";
+export const SET_USER = "login/SET_USER";
+export const UPDATE_USER = "login/UPDATE_USER";
+export const SET_LOADING = "login/SET_LOADING";
 
 const initialState = {
   token: null,
   user: null,
-  loading: true,
+  loading: true
 };
 
 export default (state = initialState, action) => {
@@ -21,134 +21,136 @@ export default (state = initialState, action) => {
     case SET_TOKEN:
       return {
         ...state,
-        token: action.payload,
+        token: action.payload
       };
     case SET_USER:
       return {
         ...state,
-        user: action.user,
+        user: action.user
       };
     case UPDATE_USER:
       return {
         ...state,
         user: {
           ...state.user,
-          ...action.user,
-        },
+          ...action.user
+        }
       };
     case SET_LOADING:
       return {
         ...state,
-        loading: action.loading,
+        loading: action.loading
       };
     default:
       return state;
   }
 };
 
-export const autoLogin = () => async (dispatch) => {
-  if (localStorage.hasOwnProperty('utt-arena-token') && localStorage.hasOwnProperty('utt-arena-userid')) { // eslint-disable-line no-prototype-builtins
-    const localToken = localStorage.getItem('utt-arena-token');
-    const userId = localStorage.getItem('utt-arena-userid');
+export const autoLogin = () => async dispatch => {
+  if (
+    localStorage.hasOwnProperty("utt-arena-token") &&
+    localStorage.hasOwnProperty("utt-arena-userid")
+  ) {
+    // eslint-disable-line no-prototype-builtins
+    const localToken = localStorage.getItem("utt-arena-token");
+    const userId = localStorage.getItem("utt-arena-userid");
     dispatch(saveToken(localToken));
     try {
       const res = await API.get(`users/${userId}`);
       dispatch({
         type: SET_USER,
-        user: res.data,
+        user: res.data
       });
-    }
-    catch (err) {
+    } catch (err) {
       dispatch({
         type: SET_LOADING,
-        loading: false,
+        loading: false
       });
 
       // Delete not working values
-      localStorage.removeItem('utt-arena-token');
-      localStorage.removeItem('utt-arena-userid');
+      localStorage.removeItem("utt-arena-token");
+      localStorage.removeItem("utt-arena-userid");
     }
   }
   dispatch({
     type: SET_LOADING,
-    loading: false,
+    loading: false
   });
 };
 
-export const tryLogin = (user) => async (dispatch) => {
-  const res = await API.post('auth/login', user);
+export const tryLogin = user => async dispatch => {
+  const res = await API.post("auth/login", user);
   dispatch(saveToken(res.data.token));
-  localStorage.setItem('utt-arena-userid', res.data.user.id);
+  localStorage.setItem("utt-arena-userid", res.data.user.id);
   const OneSignal = window.OneSignal || [];
   OneSignal.push(() => {
     OneSignal.init({
-      appId: 'cac7c5b1-3b95-4c2d-b5df-b631b3c3646b',
+      appId: "893364a5-daf6-428c-9570-77e908cd5976"
     });
   });
-  OneSignal.push(() => {
-    OneSignal.sendTags({
-      tournamentId: res.data.user.team.tournamentId,
-    });
-  });
+  //OneSignal.push(() => {
+  //  OneSignal.sendTags({
+  //    tournamentId: null //res.data.user.team.tournamentId
+  //  });
+  //});
   dispatch({
     type: SET_USER,
-    user: res.data.user,
+    user: res.data.user
   });
   dispatch(setLoginModalVisible(false));
   if (res.data.user.permissions) {
-    Router.push('/admin/entry');
-  }
-  else {
-    Router.push('/dashboard');
+    Router.push("/admin/entry");
+  } else {
+    Router.push("/dashboard");
   }
   return true;
 };
 
-export const saveToken = (token) => (dispatch) => {
+export const saveToken = token => dispatch => {
   dispatch({
     type: SET_TOKEN,
-    token,
+    token
   });
   setTokenAPI(token);
-  localStorage.setItem('utt-arena-token', token);
+  localStorage.setItem("utt-arena-token", token);
 };
 
-export const logout = async (dispatch) => {
-  toast('Vous avez été déconnecté');
+export const logout = async dispatch => {
+  toast("Vous avez été déconnecté");
   dispatch({ type: SET_TOKEN, token: null });
   dispatch({ type: SET_USER, user: null });
-  setTokenAPI('');
-  localStorage.removeItem('utt-arena-userid');
-  localStorage.removeItem('utt-arena-token');
-  Router.push('/');
+  setTokenAPI("");
+  localStorage.removeItem("utt-arena-userid");
+  localStorage.removeItem("utt-arena-token");
+  Router.push("/");
 };
 
-export const editUser = (data, userId) => async (dispatch) => {
+export const editUser = (data, userId) => async dispatch => {
   const res = await API.put(`/users/${userId}`, data);
-  toast.success('Vos informations ont été modifiées');
+  toast.success("Vos informations ont été modifiées");
   dispatch({
     type: UPDATE_USER,
-    user: res.data,
+    user: res.data
   });
 };
 
-export const resetPassword = (email, resetFields) => async (dispatch) => {
-  await API.post('auth/reset', { email });
-  toast.success('Un email de confirmation vient d\'être envoyé');
+export const resetPassword = (email, resetFields) => async dispatch => {
+  await API.post("auth/reset", { email });
+  toast.success("Un email de confirmation vient d'être envoyé");
   dispatch(setLoginModalVisible(false));
   resetFields();
 };
 
-export const setType = (type) => async (dispatch, getState) => {
+export const setType = type => async (dispatch, getState) => {
   const user = getState().login.user;
   const res = await API.put(`/users/${user.id}`, { ...user, type });
   await API.delete(`/users/${user.id}/carts/current/items`);
   dispatch({
     type: SET_USER,
-    user: res.data,
+    user: res.data
   });
   dispatch({
     type: SET_CART,
-    cart: null,
+    cart: null
   });
 };
