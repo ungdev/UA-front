5081457aa704311aa9be5564e82e83f5aa939475
Saperlu <saperli000@gmail.com>
2021-08-19 13:55:05 +0200

    Button and Card
    + .accent

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -4,19 +4,38 @@ import PropTypes from 'prop-types';
 /**
  * Displays a button that triggers an action when clicked
  */
-const Button = ({ primary, onClick, children, type, leftIcon, rightIcon, disabled, noStyle, className }) => (
-  <button
-    type={type}
-    className={`button ${className} ${primary ? 'primary' : ''} ${noStyle ? 'no-style' : ''} ${
-      !children ? 'empty' : ''
-    }`}
-    onClick={onClick}
-    disabled={disabled}>
-    {leftIcon && <i className={`button-icon-left ${leftIcon}`} />}
-    {children}
-    {rightIcon && <i className={`button-icon-right ${rightIcon}`} />}
-  </button>
-);
+const Button = ({ primary, onClick, children, type, leftIcon, rightIcon, disabled, noStyle, className }) => {
+  // eslint-disable-next-line no-console
+  console.log(typeof children);
+  // if (typeof children === 'string') {
+  //   const firstLetter = children[0];
+  //   const text = children.slice(1);
+  //   children = (
+  //     <>
+  //       <span className="yellow">{firstLetter}</span>
+  //       {text}
+  //     </>
+  //   );
+  // }
+
+  return (
+    <div className={`buttonWrapper ${className} ${noStyle ? 'no-style' : ''} ${disabled ? 'disabled' : ''}`}>
+      <div className={`topButtonDiv ${className} ${noStyle ? 'no-style' : ''} ${disabled ? 'disabled' : ''}`}>
+        <div className={`bottomButtonDiv ${noStyle ? 'no-style' : ''} ${disabled ? 'disabled' : ''}`}>
+          <button
+            type={type}
+            className={`button ${primary ? 'primary' : ''} ${noStyle ? 'no-style' : ''} ${!children ? 'empty' : ''}`}
+            onClick={onClick}
+            disabled={disabled}>
+            {leftIcon && <i className={`button-icon-left ${leftIcon}`} />}
+            {children}
+            {rightIcon && <i className={`button-icon-right ${rightIcon}`} />}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 Button.propTypes = {
   /**
