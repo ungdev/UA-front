7aa4dcabffaf7e6caeb96214f60356031351acc8
Valentin Peltier <valentinpeltier@yahoo.fr>
2021-10-11 09:11:36 +0200

    feat: add organisers page (#194)

--- src/pages/information.jsx
+++ src/pages/information.jsx
@@ -28,11 +28,11 @@ const Information = () => (
       les joueurs, coachs, simples curieux… qui nous faites confiance chaque année pour organiser cet événement dans
       l'ambiance qu'on lui connaît ! La confiance de nos partenaires et leur mobilisation autour de notre événement
       contribuent à le rendre exceptionnel ! Un grand merci également à{' '}
-      {/* <Link href="/organisers">
-        <a> */}
-      <span className="accent">tous les bénévoles</span>{' '}
-      {/* </a>
-      </Link>{' '} */}
+      <Link href="/organisers">
+        <a>
+          <span className="accent">tous les bénévoles</span>
+        </a>
+      </Link>{' '}
       qui rendent possible l'organisation de ce beau projet.
     </div>
 
--- src/pages/organisers.tsx
+++ src/pages/organisers.tsx
@@ -4,7 +4,7 @@ import { Title, Loader } from '../components/UI';
 import { Organisers as OrganisersType } from '../types';
 import { uploads } from '../utils/api';
 import { uploadsUrl } from '../utils/environment';
-import { getShortName } from '../utils/strings';
+import { getOrganiserShortName } from '../utils/strings';
 
 const Organisers = () => {
   const [organisers, setOrganisers] = useState<OrganisersType | null>(null);
@@ -24,50 +24,47 @@ const Organisers = () => {
       <Title align="center" className="uppercase">
         Organisateurs
       </Title>
+
+      <p>
+        Merci à tous les organisateurs qui permettent à ce magnifique événement d'exister !<br />
+        Ils sont tous bénévoles et étudiants à l'Université de Technologie de Troyes (UTT).
+      </p>
+
+      {!organisers ? (
+        <Loader />
+      ) : (
+        organisers.map((commission) => (
+          <div className="commission" key={commission.title}>
+            <Title level={2}>{commission.title}</Title>
+            <p>{commission.description}</p>
+
+            <div className="members">
+              {commission.members.map((member) => (
+                <div className="member" key={member.name}>
+                  <img
+                    src={`${uploadsUrl()}/organisers/${getOrganiserShortName(member.name)}.jpg`}
+                    onError={(e) => {
+                      const imageElement = e.target as HTMLImageElement;
+
+                      if (imageElement.src !== '/question-mark.png') {
+                        imageElement.onerror = null;
+                        imageElement.src = '/question-mark.png';
+                      }
+                    }}
+                  />
+                  <div className="name">
+                    {member.name}
+                    {member.pseudo ? ` (${member.pseudo})` : ''}
+                  </div>
+                  <div className="role">{member.role}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))
+      )}
     </div>
   );
-
-  // return (
-  //   <div id="organisers">
-  //     <Title align="center" className="uppercase">
-  //       Organisateurs
-  //     </Title>
-
-  //     <p>
-  //       Merci à tous les organisateurs qui permettent à ce magnifique événement d'exister. Ils sont tous bénévoles et
-  //       étudiants à l'Université de Technologie de Troyes (UTT).
-  //     </p>
-
-  //     {!organisers ? (
-  //       <Loader />
-  //     ) : (
-  //       organisers.map((commission) => (
-  //         <div className="commission">
-  //           <Title level={2}>{commission.title}</Title>
-  //           <p>{commission.description}</p>
-
-  //           <div className="members">
-  //             {commission.members.map((member) => (
-  //               <div className="member">
-  //                 <div
-  //                   className="image"
-  //                   style={{
-  //                     backgroundImage: `url(${uploadsUrl()}/organisers/${getShortName(
-  //                       member.name,
-  //                     )}.jpg), url(/question-mark.png)`,
-  //                   }}
-  //                 />
-  //                 <div className="name">{member.name}</div>
-  //                 <div>{member.pseudo}</div>
-  //                 {/* <div className="role">{member.role}</div> */}
-  //               </div>
-  //             ))}
-  //           </div>
-  //         </div>
-  //       ))
-  //     )}
-  //   </div>
-  // );
 };
 
 export default Organisers;
