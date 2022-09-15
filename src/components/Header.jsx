393614789a06157975b7ef6fa500d116bfcff8b6
AlbanSdl <alban.delavoreille@free.fr>
2022-09-15 22:56:50 +0200

    fix: update partial cgv content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,10 @@ const Header = () => (
   <header id="header">
     <div className="home-title">
       <p className="main">UTT Arena</p>
-      <p>2, 3 et 4 décembre 2022</p>
+      <p>
+        <span className="accent">2</span>, <span className="accent">3</span> et <span className="accent">4</span>{' '}
+        décembre 2022
+      </p>
     </div>
   </header>
 );
