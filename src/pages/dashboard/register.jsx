00b76fad1ab22e47496ff2127f95f37fd043b0f6
Noé Landré <noe.landre@laposte.net>
2022-09-27 21:38:53 +0200

    feat: add tournaments cards

--- src/pages/dashboard/register.jsx
+++ src/pages/dashboard/register.jsx
@@ -9,21 +9,53 @@ import Link from 'next/link';
 import { API } from '../../utils/api';
 
 const Register = () => {
+  const { query } = useRouter();
   const user = useSelector((state) => state.login.user);
   const [discordLink, setDiscordLink] = useState('');
   const discordLinkRef = useRef(null);
+  const [userType, setUserType] = useState('');
   const [step, setStep] = useState(1);
+  const soloTeamName = `${user.username}-solo-team`;
+  const [tournaments, setTournaments] = useState([]);
+
+  // Split multiplayer and solo tournaments
+  const tournamentsList = tournaments.filter((tournament) => tournament.playersPerTeam > 1);
+  const tournamentsSoloList = tournaments.filter((tournament) => tournament.playersPerTeam === 1);
+
+  // If there's a query, pre-select the tournament given by this query
+  if (query.tournamentId && (tournamentId !== query.tournamentId || tournamentSolo !== query.tournamentId)) {
+    if (tournamentsList.filter((tournament) => tournament.id === query.tournamentId)) {
+      setTournamentId(query.tournamentId);
+    }
+    if (tournamentsSoloList.filter((tournament) => tournament.id === query.tournamentId)) {
+      setTournamentSolo(query.tournamentId);
+    }
+  }
+
+  // Get tournaments category select options
+  const tournamentsOptions = tournamentsList.map((tournament) => ({
+    label: tournament.name,
+    value: tournament.id,
+  }));
+
+  const tournamentsSoloOptions = tournamentsSoloList.map((tournament) => ({
+    label: tournament.name,
+    value: tournament.id,
+  }));
 
   useEffect(() => {
     API.get('discord/connect').then((res) => {
       setDiscordLink(res.data.link);
     });
 
+    (async () => {
+      setTournaments((await API.get('/tournaments')).data);
+    })();
+
     if(user.discordId) setStep(2);
 
     setStep(2);
   }, []);
-
 
   const Step1 = (
     <>
@@ -37,6 +69,7 @@ const Register = () => {
     <>
       <div className="card-container">
         <div onClick={() => {
+          setUserType('player')
           setStep(3);
         }}>
           <i className="fa fa-gamepad"></i>
@@ -44,6 +77,7 @@ const Register = () => {
         </div>
 
         <div onClick={() => {
+          setUserType('coach')
           setStep(3);
         }}>
           <i className="fa fa-headset"></i>
@@ -53,8 +87,35 @@ const Register = () => {
     </>
   );
 
+  const loadTournaments = () => {
+    let list = tournamentsOptions.map((element,i) => {
+      return (<div key={i} onClick={() => {
+        setTournamentId(element.value)
+        setStep(4);
+      }}>
+        <p>{element.label}</p>
+      </div>);
+    })
+
+    if (userType === 'player') {
+      list.push(tournamentsSoloOptions.map((element,i) => {
+        return (<div key={i} onClick={() => {
+          setTournamentSolo(element.value)
+          setStep(4);
+        }}>
+          <p>{element.label}</p>
+        </div>);
+      }));
+    }
+    return list;
+  }
+
   const Step3 = (
-    <></>
+    <>
+      <div className="card-container">
+        { loadTournaments() }
+      </div>
+    </>
   );
 
   const renderSwitch = (param) => {
