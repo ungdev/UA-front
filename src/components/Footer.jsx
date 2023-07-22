d73770f8ec4944a005baf3c815d5c1c7cd2c2593
Teddy Roncin <teddy.roncin35@gmail.com>
2023-07-22 12:45:57 +0200

    added legal links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,25 @@
 import logo from '../../public/logo-footer.png'
 import Button from './UI/Button';
+import { useRouter } from "next/router";
 
-const Footer = () => (
-  <footer>
-    <img src={logo.src} alt="Logo UA23" />
-    <div className="middle">
-      <h3>Informations légales</h3>
-      <Button>Mentions légales</Button> <br />
-      <Button>Politique de confidentialité</Button>
-    </div>
-    <div className="right">
-      <h3>Contact</h3>
-      <Button>FAQ</Button>
-      <div className="lecarreblanc"></div>
-    </div>
-  </footer>
-);
+const Footer = () => {
+  const router = useRouter();
+
+  return (
+    <footer>
+      <img src={logo.src} alt="Logo UA23" />
+      <div className="middle">
+        <h3>Informations légales</h3>
+        <Button onClick={() => router.push('/legal')}>Mentions légales</Button> <br />
+        <Button onClick={() => router.push('/legal')}>Politique de confidentialité</Button>
+      </div>
+      <div className="right">
+        <h3>Contact</h3>
+        <Button>FAQ</Button>
+        <div className="lecarreblanc"></div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
