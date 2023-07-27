f5ae56c9df695b1b64aeb104408b5f5f1890fb12
Teddy Roncin <teddy.roncin35@gmail.com>
2023-07-27 03:52:17 +0200

    added a first test

--- src/components/UI/Select.stories.tsx
+++ src/components/UI/Select.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Select from './Select';
+import { userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
 const meta = {
   title: 'UI/Select',
@@ -10,7 +12,10 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+let _value = 'option1';
+
 export const Default: Story = {
+  //_value: 'option1',
   args: {
     label: 'Select',
     options: [
@@ -18,9 +23,17 @@ export const Default: Story = {
       { label: 'Option 2', value: 'option2' },
       { label: 'Option 3', value: 'option3' },
     ],
-    value: 'option1',
-    onChange: (value: string) => console.log(value),
+    value: _value,
+    onChange: (value: string) => (_value = value),
     disabled: false,
     className: 'class',
+    id: 'test',
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const select = canvas.getByLabelText('Select');
+    await expect(select).toBeInTheDocument();
+    await userEvent.selectOptions(select, 'option2');
+    await expect(_value).toBe('option2');
   },
 };
