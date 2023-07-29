39bdae5b90d24d5e759784e211d260aa58ee8928
Noé Landré <noe.landre@laposte.net>
2023-07-29 13:47:18 +0200

    feat: add burger menu content

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,36 +9,57 @@ import { useState } from 'react';
 export default function Navbar() {
   const [isBurgerOpen, setIsBurgerOpen] = useState(false);
 
+  const closeBurger = () => {
+    if(!isBurgerOpen) return;
+    setIsBurgerOpen(false);
+  }
+
+  const leftContent = (
+    <>
+      <Link href="/" onClick={closeBurger}>
+        <Button className="home">Accueil</Button>
+      </Link>
+      <Link href="/event" onClick={closeBurger}>
+        <Button className="event">Événement</Button>
+      </Link>
+      <Link href="/tournaments" onClick={closeBurger}>
+        <Button className="tournament">Tournois</Button>
+      </Link>
+      <Link href="/help" onClick={closeBurger}>
+        <Button className="help">Aide</Button>
+      </Link>
+    </>
+  );
+
+  const rightContent = (
+    <>
+      <Link href="/about" onClick={closeBurger}>
+        <Button className="about">A propos</Button>
+      </Link>
+
+      <Button primary className="connection" onClick={closeBurger}>
+        Connexion
+      </Button>
+    </>
+  );
+
   return (
     <nav>
       <div className="left">
-        <Link href="/">
-          <Button className="home">Accueil</Button>
-        </Link>
-        <Link href="/event">
-          <Button className="event">Événement</Button>
-        </Link>
-        <Link href="/tournaments">
-          <Button className="tournament">Tournois</Button>
-        </Link>
-        <Link href="/help">
-          <Button className="help">Aide</Button>
-        </Link>
+        {leftContent}
       </div>
       <div className="right">
-        <Link href="/about">
-          <Button className="about">A propos</Button>
-        </Link>
-
-        <Button primary className="connection">
-          Connexion
-        </Button>
+        {rightContent}
 
         <div className={"burger " + (isBurgerOpen ? 'open' : '')} onClick={() => setIsBurgerOpen(!isBurgerOpen)}>
           <span></span>
           <span></span>
           <span></span>
         </div>
+      </div>
+      <div className={"burger-menu-content " + (isBurgerOpen ? 'open' : '')}>
+        {leftContent}
+        {rightContent}
       </div>
     </nav>
   );
