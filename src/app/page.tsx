f3eda21d6f96bbbfc3a2499f9b979f9e12fbf54c
Teddy Roncin <teddy.roncin35@gmail.com>
2023-07-31 04:30:23 +0200

    Removed old parallax and added a wayyyyy better one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,11 @@ import Slider from '@/components/landing/Slider';
 import Counter from '@/components/landing/Counter';
 import BoxContainer from '@/components/landing/BoxContainer';
 import { VerticalDivider, Button, Title, Icon, VideoContainer } from '@/components/UI';
-// import Parallax from '@/components/UI/Parallax';
+import AppearFromSide from '@/components/UI/AppearFromSide';
 
 const Home = () => {
   return (
     <div id="home">
-      {/* <Parallax /> */}
       <Slider
         slides={[
           <div key={'slide-1'} className="home-header">
@@ -31,54 +30,61 @@ const Home = () => {
         autoslide={false}
       />
 
-      <div className="cta-main">
-        <Title level={1}>LA PLUS GRANDE COMPÉTITION E-SPORT DU GRAND EST</Title>
-        <div className="cta-main-subtitle">Prépare toi à entrer dans l’arène !</div>
-        <Button primary className="connection">
-          Découvrir l’événement
-        </Button>
-      </div>
+      <AppearFromSide>
+        <div className="cta-main">
+          <Title level={1}>LA PLUS GRANDE COMPÉTITION E-SPORT DU GRAND EST</Title>
+          <div className="cta-main-subtitle">Prépare toi à entrer dans l’arène !</div>
+          <Button primary className="connection">
+            Découvrir l’événement
+          </Button>
+        </div>
+      </AppearFromSide>
 
-      <BoxContainer title="chiffres.txt">
-        <Counter value={7} name="Jeux" className="flex-1" />
-        <VerticalDivider />
-        <Counter value={10000} valueText=" €" name="de cashprize" className="flex-2" />
-        <VerticalDivider />
-        <Counter value={500} name="Participants" className="flex-1" />
-      </BoxContainer>
-      <div className="information">
-        <div className="information-container">
-          <div className="left">
-            <Title level={1}>UTT Arena 2023</Title>
-            <p className="information-text">
-              LE rendez-vous gaming annuel de la région Auboise aura lieu les 3 et 4 décembre 2022 dans un lieu que vous
-              connaissez bien : le Cube ! Plus de 2250m2 t'attendent pour passer 2 jours de folie ! <br />
-              Au programme : 7 tournois sur tes jeux favoris, 2 jours pour montrer tes skills parmis les 630 joueurs qui
-              composeront l’évènement, et tenter de remporter les cashprizes, lots et de nombreuses autres surprises !
-              Et pour animer cette édition, des guests d’exception viendront caster cette édition qui s’annonce
-              enflammée ! Alors prépare tout ton stuff et impose toi dans l’arène !
-            </p>
+      <AppearFromSide fromRight>
+        <BoxContainer title="chiffres.txt">
+          <Counter value={7} name="Jeux" className="flex-1" />
+          <VerticalDivider />
+          <Counter value={10000} valueText=" €" name="de cashprize" className="flex-2" />
+          <VerticalDivider />
+          <Counter value={500} name="Participants" className="flex-1" />
+        </BoxContainer>
+      </AppearFromSide>
+
+      <AppearFromSide>
+        <div className="information">
+          <div className="information-container">
+            <div className="left">
+              <Title level={1}>UTT Arena 2023</Title>
+              <p className="information-text">
+                LE rendez-vous gaming annuel de la région Auboise aura lieu les 3 et 4 décembre 2022 dans un lieu que
+                vous connaissez bien : le Cube ! Plus de 2250m2 t'attendent pour passer 2 jours de folie ! <br />
+                Au programme : 7 tournois sur tes jeux favoris, 2 jours pour montrer tes skills parmis les 630 joueurs
+                qui composeront l’évènement, et tenter de remporter les cashprizes, lots et de nombreuses autres
+                surprises ! Et pour animer cette édition, des guests d’exception viendront caster cette édition qui
+                s’annonce enflammée ! Alors prépare tout ton stuff et impose toi dans l’arène !
+              </p>
+            </div>
+            <div className="right">
+              <BoxContainer title="image.jpg" padding={false}>
+                <img src="https://picsum.photos/seed/add/1920/1080/" alt="Information Image" className="" />
+              </BoxContainer>
+              <BoxContainer title="image.jpg" padding={false}>
+                <img src="https://picsum.photos/1920/1080" alt="Information Image" className="" />
+              </BoxContainer>
+            </div>
           </div>
-          <div className="right">
-            <BoxContainer title="image.jpg" padding={false}>
-              <img src="https://picsum.photos/seed/add/1920/1080/" alt="Information Image" className="" />
-            </BoxContainer>
-            <BoxContainer title="image.jpg" padding={false}>
-              <img src="https://picsum.photos/1920/1080" alt="Information Image" className="" />
-            </BoxContainer>
+          <div className="more-info">
+            <div className="more-info-container">
+              <Icon name="map-pin" fill={false} />
+              TROYES
+            </div>
+            <div className="more-info-container">
+              <Icon name="calendar" fill={false} />
+              1, 2, 3 Décembre 2023
+            </div>
           </div>
         </div>
-        <div className="more-info">
-          <div className="more-info-container">
-            <Icon name="map-pin" fill={false} />
-            TROYES
-          </div>
-          <div className="more-info-container">
-            <Icon name="calendar" fill={false} />
-            1, 2, 3 Décembre 2023
-          </div>
-        </div>
-      </div>
+      </AppearFromSide>
       <Partners />
     </div>
   );
---
+++ src/components/UI/AppearFromSide.tsx
@@ -0,0 +1,51 @@
+'use client';
+import React, { useEffect, useRef, useState } from 'react';
+
+export default function AppearFromSide({ children, fromRight, deactivated = false }) {
+  const [visible, setVisible] = useState(false);
+  const [translateXData, setTranslateXData] = useState({ offset: 0, position: 0, lastOffset: 0 });
+  const ref = useRef();
+
+  const animation = () => {
+    const rect = ref.current.getBoundingClientRect();
+    const progression = Math.max(Math.min(2 * (1 - rect.top / window.innerHeight), 1), 0);
+    const defaultSidePosition = translateXData.position - translateXData.lastOffset;
+    setTranslateXData({
+      offset: fromRight
+        ? (1 - progression) * (window.innerWidth - defaultSidePosition)
+        : (progression - 1) * defaultSidePosition,
+      position: fromRight ? rect.left : rect.right,
+      lastOffset: translateXData.offset,
+    });
+  };
+
+  useEffect(() => {
+    new IntersectionObserver(
+      ([entry]) => {
+        setVisible(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0,
+      } as IntersectionObserverInit,
+    ).observe(ref.current!);
+    const rect = ref.current.getBoundingClientRect();
+    setTranslateXData({
+      offset: fromRight ? window.innerWidth - rect.left : -rect.right,
+      position: fromRight ? rect.left : rect.right,
+      lastOffset: 0,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (visible && !deactivated) {
+      requestAnimationFrame(animation);
+    }
+  }, [visible, translateXData]);
+
+  return React.cloneElement(children, {
+    style: deactivated ? undefined : { transform: `translateX(${translateXData.offset}px)` },
+    ref,
+  });
+}
