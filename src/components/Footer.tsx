270bcedb1c76da88ee29ebe73c9ad2ffbba51e0c
ploine <pauline.bergerot@hotmail.fr>
2023-08-21 18:59:17 +0200

    move copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,47 +10,51 @@ import { Icon } from './UI';
 export default function Footer() {
   return (
     <footer>
-      <div>
-        <img src={logo.src} alt="Logo UA23" />
-      </div>
-      <div className="middle">
-        <h3>Informations légales</h3>
-        <Link href={'/legal'}>
-          <Button>Mentions légales</Button>
-        </Link>
-        <Link href={'/legal'}>
-          <Button>Politique de confidentialité</Button>
-        </Link>
-        <Button>© {new Date().getFullYear()} UTT Net Group</Button>
-      </div>
-      <div className="right">
-        <h3>Plus d'informations</h3>
-        <Link href={'/help#contact'}>
-          <Button>Nous contacter</Button>
-        </Link>
-        <Link href={'/help#faq'}>
-          <Button>FAQ</Button>
-        </Link>
-        <div className="socials">
-          <Link href={'https://discord.gg/WhxZwKU'}>
-            <Icon name="discord" fill={true} />
+      <div className='row'>
+        <div>
+          <img src={logo.src} alt="Logo UA23" />
+        </div>
+        <div className="middle">
+          <h3>Informations légales</h3>
+          <Link href={'/legal'}>
+            <Button>Mentions légales</Button>
           </Link>
-          <Link href={'https://www.twitch.tv/uttarena'}>
-            <Icon name="twitch" fill={true} />
-          </Link>
-          <Link href={'https://www.youtube.com/user/UTTNetGroup/'}>
-            <Icon name="youtube" fill={true} />
-          </Link>
-          <Link href={'https://www.instagram.com/uttarena/'}>
-            <Icon name="instagram" fill={true} />
-          </Link>
-          <Link href={'https://www.facebook.com/UTTArena'}>
-            <Icon name="facebook" fill={true} />
-          </Link>
-          <Link href={'https://twitter.com/uttarena'}>
-            <Icon name="twitter" fill={true} />
+          <Link href={'/legal'}>
+            <Button>Politique de confidentialité</Button>
           </Link>
         </div>
+        <div className="right">
+          <h3>Plus d'informations</h3>
+          <Link href={'/help#contact'}>
+            <Button>Nous contacter</Button>
+          </Link>
+          <Link href={'/help#faq'}>
+            <Button>FAQ</Button>
+          </Link>
+          <div className="socials">
+            <Link href={'https://discord.gg/WhxZwKU'}>
+              <Icon name="discord" fill={true} />
+            </Link>
+            <Link href={'https://www.twitch.tv/uttarena'}>
+              <Icon name="twitch" fill={true} />
+            </Link>
+            <Link href={'https://www.youtube.com/user/UTTNetGroup/'}>
+              <Icon name="youtube" fill={true} />
+            </Link>
+            <Link href={'https://www.instagram.com/uttarena/'}>
+              <Icon name="instagram" fill={true} />
+            </Link>
+            <Link href={'https://www.facebook.com/UTTArena'}>
+              <Icon name="facebook" fill={true} />
+            </Link>
+            <Link href={'https://twitter.com/uttarena'}>
+              <Icon name="twitter" fill={true} />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className='copyright'>
+        <Button>© {new Date().getFullYear()} UTT Net Group</Button>
       </div>
     </footer>
   );
