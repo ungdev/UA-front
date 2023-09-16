3b603d6c0a54f689a8ecc420244daa1118716f43
Teddy Roncin <teddy.roncin35@gmail.com>
2023-09-16 16:32:10 +0200

    implemented basic badge creation

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -79,6 +79,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     }
 
     menu.push({ title: 'Compte', href: '/admin/account' });
+    menu.push({ title: 'Badge', href: '/admin/badge' });
 
     return menu;
   };
