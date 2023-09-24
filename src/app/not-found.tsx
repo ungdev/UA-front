1b0ce594c35733098de3a2655bd998052233cb2a
Teddy Roncin <teddy.roncin35@gmail.com>
2023-09-24 03:24:55 +0200

    style: page 404 is now wayy cooler

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,4 @@
+'use client';
 import styles from './not-found.module.scss';
 import Link from 'next/link';
 
@@ -8,13 +9,25 @@ export default function NotFound() {
     <>
       <div id="not-found" className={styles.notFound}>
         <Title level={1} type={1} align="center">
-          Page introuvable
+          Erreur 404
         </Title>
-        <p>Erreur 404</p>
+        <p>
+          La page <span>{window.location.pathname}</span> n’existe pas.
+          <br />
+          <br />
+          Si tu penses que c’est une erreur, contacte le staff
+        </p>
 
-        <Link href="/">
-          <Button primary>Retour à l'accueil</Button>
-        </Link>
+        <div className={styles.buttons}>
+          <Link href="/">
+            <Button primary>Retour à l'accueil</Button>
+          </Link>
+          {document.referrer !== window.location.href && (
+            <Link href={document.referrer}>
+              <Button primary>Retour à la page précédente</Button>
+            </Link>
+          )}
+        </div>
       </div>
     </>
   );
