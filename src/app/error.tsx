60720e719b1716ea8f4092222082f06d0e31aa50
Leo Cholvy <leo.cholvy@utt.fr>
2023-10-02 21:37:48 +0200

    Page erreur

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useEffect } from 'react';
 import Link from 'next/link';
+import styles from './not-found.module.scss';
 
 import { Title, Button } from '@/components/UI';
 
@@ -10,20 +11,47 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
     console.error(error);
   }, [error]);
 
+  if (error.name) {
+    console.log(error);
+  }
+
+  function copyError() {
+    // Copy the error to the clipboard
+    const textArea = document.createElement('textarea');
+    textArea.value = error.toString();
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+  }
+
   return (
     <>
-      <Title level={1} type={2}>
-        Une erreur est survenue...
-      </Title>
-      <p>{error.name && `Erreur ${error.name}`}</p>
+      <div id={styles.notFound}>
+        <Title level={1} type={1} align="center">
+          Une erreur est survenue...
+        </Title>
+        {/* <p>{error.name && `Erreur ${error.name}`}</p> */}
+        <p>Si tu penses que ce n'est pas ta faute, contacte le staff.</p>
 
-      <Button primary onClick={reset}>
-        Réessayer
-      </Button>
+        <Button onClick={copyError}>Copier l'erreur</Button>
 
-      <Link href="/">
-        <Button primary>Retour à l'accueil</Button>
-      </Link>
+        <div className={styles.buttons}>
+          <Link href={window.location.href}>
+            <Button primary onClick={reset}>
+              Réessayer
+            </Button>
+          </Link>
+
+          <Link href="/">
+            <Button primary>Retour à l'accueil</Button>
+          </Link>
+        </div>
+
+        <div className={styles.grass}>
+          <p>C'est l'heure de toucher de l'herbe...</p>
+        </div>
+      </div>
     </>
   );
 }
