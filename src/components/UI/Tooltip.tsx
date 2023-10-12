293d975acf684a7e51e75ffee76a91f6042e05f7
Teddy Roncin <teddy.roncin@proton.com>
2023-10-12 19:53:05 +0200

    docs: documented Tooltip component

--- src/components/UI/Tooltip.tsx
+++ src/components/UI/Tooltip.tsx
@@ -6,8 +6,11 @@ export default function Tooltip({
   enabled = true,
   children,
 }: {
+  /** The string that will be displayed. It should not be too long, it would look bad otherwise, until it is fixed. */
   tooltip: string;
+  /** Whether this toolbox is enabled or not. If set to false, it will act as a normal <div>. */
   enabled?: boolean;
+  /** The object that should be tooltiped. */
   children: ReactNode;
 }) {
   if (!enabled) {
