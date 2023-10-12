4f0ed6be5881ba8b20d3416ef043ac34e2a3328d
Noé Landré <noe.landre@laposte.net>
2023-10-12 14:46:33 +0200

    feat: start uncommenting things

--- src/app/legal/page.tsx
+++ src/app/legal/page.tsx
@@ -61,7 +61,7 @@ const Legal = () => (
         directive européenne 2009/136/CE, ces cookies sont indispensables à la fourniture du service sollicité. En
         naviguant sur notre site web, vous acceptez l’utilisation de ces cookies.
       </p>
-      {/*<Title align="center" id="CGV">
+      <Title align="center" id="CGV">
         Conditions générales de vente
       </Title>
       <Title level={2} type={2} className={styles.articleTitle}>
@@ -344,7 +344,7 @@ const Legal = () => (
         <br />
         Email : ung@utt.fr
         <br />
-      </p>*/}
+      </p>
     </div>
   </div>
 );
