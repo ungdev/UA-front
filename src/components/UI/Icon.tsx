e256388ae345dc2f335f5879803e01a4d3516976
Noé Landré <noe.landre@laposte.net>
2023-10-17 21:19:57 +0200

    feat: add signout icon

--- src/components/UI/Icon.tsx
+++ src/components/UI/Icon.tsx
@@ -28,6 +28,7 @@ export enum IconName {
   User = 'user',
   PlusSquare = 'plus-square',
   SignIn = 'signin',
+  SignOut = 'signout',
   QuestionCircle = 'question-circle',
 }
 
@@ -269,6 +270,11 @@ const Icon = ({
           strokeLinejoin="round"
           d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
         />
+      );
+      break;
+    case 'signout':
+      icon = (
+        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
       );
       break;
 
