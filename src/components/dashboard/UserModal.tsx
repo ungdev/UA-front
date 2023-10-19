cb104dece62aa16191b81f5ae386e6f3fde4b80d
Noé Landré <noe.landre@laposte.net>
2023-10-20 01:44:45 +0200

    fix: usermodal

--- src/components/dashboard/UserModal.tsx
+++ src/components/dashboard/UserModal.tsx
@@ -292,10 +292,10 @@ const UserModal = ({
               <div className={styles.checkboxContainer}>
                 {permissionOptions.map((option) => (
                   <Checkbox
-                    key={option.value}
+                    key={option.name}
                     label={option.name}
                     value={
-                      permissions.find((permission) => permission === (option.value as unknown as Permission)) !== null
+                      permissions.find((permission) => permission === (option.value as unknown as Permission)) != null
                     }
                     onChange={(v) => {
                       if (v) {
