6acf95a8e2223e66028a0a4096b1504eb3256390
Antoine D <antoine.dufils@yahoo.fr>
2024-09-16 15:48:28 +0200

    It "works" but can't find tournament ids

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -3,15 +3,31 @@ import fs from 'fs';
 import path from 'path';
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const tournamentId = ['lol', 'cs2', 'osu'];
   // Path to the directory containing your TSX files
   const siteDirectory = path.join(process.cwd(), '/src/app');
 
   // Retrieve all file paths recursively
   const filePaths = getAllFilePaths(siteDirectory);
 
+  const absoluteAppPaths = path.resolve('./src');
+
+  const allowedFilePaths = filePaths.filter((filePath) => {
+    const relativeFilePaths = path.relative(absoluteAppPaths, filePath);
+    if (
+      relativeFilePaths.slice(3, 15) !== '/(dashboard)' &&
+      relativeFilePaths.slice(3, 9) !== '/oauth' &&
+      relativeFilePaths.slice(3, 9) !== '/reset' &&
+      relativeFilePaths.slice(3, 12) !== '/validate' &&
+      relativeFilePaths.slice(3, 12) !== '/page.tsx'
+    ) {
+      return filePath;
+    }
+  });
+
   // Select only .tsx files
-  const tsxFilePaths = filePaths.filter((filePath) => {
-    if (filePath.slice(-4) === '.tsx') {
+  const tsxFilePaths = allowedFilePaths.filter((filePath) => {
+    if (filePath.slice(-8) === 'page.tsx' && filePath.indexOf('[') === -1) {
       return filePath;
     }
   });
@@ -22,22 +38,31 @@ export default function sitemap(): MetadataRoute.Sitemap {
     const url = `${process.env.NEXT_PUBLIC_URL}/${category}`;
     const changeFrequency = 'weekly' as const;
     const lastModified = fs.statSync(filePath).mtime;
+    const priority = 0.5;
     return {
       url,
       lastModified,
       changeFrequency,
+      priority,
     };
   });
 
   // Add other URLs to the sitemap
-  sitemap.push(
-    {
-      url: `${process.env.NEXT_PUBLIC_URL}`,
+  for (const id of tournamentId) {
+    sitemap.push({
+      url: `${process.env.NEXT_PUBLIC_URL}/tournament/${id}`,
       lastModified: new Date(),
       changeFrequency: 'weekly' as const,
-    },
-    // Add other URLs here
-  );
+      priority: 1.0,
+    });
+  }
+
+  sitemap.push({
+    url: `${process.env.NEXT_PUBLIC_URL}`,
+    lastModified: new Date(),
+    changeFrequency: 'weekly' as const,
+    priority: 0.8,
+  });
 
   return sitemap;
 }
