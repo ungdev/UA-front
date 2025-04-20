8762c0e9bed94836995706676e26dcb1000ea43e
Antoine D <antoine.dufils@yahoo.fr>
2025-04-20 10:56:59 +0200

    fix: delete argTypeRegex which is deprecated and fix onClick in the button stories (need to do that for all stories)

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -7,7 +7,6 @@ import '@/styles.scss';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
